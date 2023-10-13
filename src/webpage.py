ee5dee26bac094d10e58180ad87be9b6a255ac17
JoernStoehler <124216958+JoernStoehler@users.noreply.github.com>
2023-10-13 19:34:04 +0200

    mockup project

---
+++ src/webpage.py
@@ -0,0 +1,8 @@
+# generate the static webpage docs/generated/<id>.html
+# by reading docs/generated/<id>.json
+# and filling in the template templates/webpage.html
+
+# the final webpage embeds the image docs/generated/<id>.png
+
+def generate_webpage(id: str):
+    path_json = 
